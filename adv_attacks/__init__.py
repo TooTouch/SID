aee0c0724e03f2bb79247fbc7f2c2472e70fbc0a
tootouch <wogur379@gmail.com>
2022-10-26 11:14:50 +0900

    [adv attack] add AutoAttack and Square

--- adv_attacks/__init__.py
+++ adv_attacks/__init__.py
@@ -1,2 +1,1 @@
-from .sid import *
 from .factory import *
--- adv_attacks/factory.py
+++ adv_attacks/factory.py
@@ -1,9 +1,11 @@
+
+
 def create_attack(model, adv_method, adv_params, **kwargs):
     for k in ['eps','alpha','step_size']:
         if k in adv_params.keys():
             adv_params[k] = eval(adv_params[k])
 
-    if adv_method == 'FAB':
+    if adv_method in ['FAB','AutoAttack']:
         assert kwargs.get('num_classes',False), 'FAB needs to define the number of classes.'
         adv_params['n_classes'] = kwargs['num_classes']
 
